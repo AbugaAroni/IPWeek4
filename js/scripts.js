0d99ba96d883c34daab06232c195521294287afc
AbugaAroni <abugaaroni@gmail.com>
2020-08-03 16:18:49 +0300

    add action and call to action when delivery button is clicked and information submitted

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,23 @@
 
 $(document).ready(function() {
 
- //when delivery button is clicked, the address collection div should show
+ //when delivery button is clicked, the address collection div should show and hide instore pick up button
 $("#delivery").click( function(event) {
   event.preventDefault();
   $("#addressbox").removeClass('show-div');
+  $("button#pickup").addClass('show-div');
 });
 
-//when inpick up button is clicked, the address collection div should show
-$("#pickup").click( function(event) {
+
+
+//when address info is submitted, this prompt appears. Address box will be hidden and the order box will appear
+$("form#address-form").submit(function(event) {
  event.preventDefault();
- $("#").removeClass('show-div');
+ alert("Your order will be delivered to your location! Make your order now");
+  $("#order").removeClass('show-div');
+  $("#addressbox").addClass('show-div');
+  $("button#delivery").addClass('show-div');
+  $("p#del-message").removeClass('show-div');
 });
 
 
