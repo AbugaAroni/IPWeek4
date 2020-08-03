d7f049216469fbb2212392b508a430d958b83e1e
AbugaAroni <abugaaroni@gmail.com>
2020-08-03 17:13:57 +0300

    add form capturing infromation for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Business interface logic
-function Account (name,balance) {
+function Pizza (size,crust,cheese,toppings,custom) {
   this.FullName = name;
   this.InitialBalance = balance;
 }
@@ -34,7 +34,30 @@ $("form#address-form").submit(function(event) {
   $("#addressbox").addClass('show-div');
   $("button#delivery").addClass('show-div');
   $("p#del-message").removeClass('show-div');
+});
 
+//when pizzaorder is submitted
+$("form#pizza-form").submit(function(event) {
+ event.preventDefault();
+
+ var inputtedPSize = $("input#new-name").val();
+ var inputtedPCrust = $("input#new-name").val();
+ var inputtedPCheese = $("input#new-name").val();
+ var inputtedPType = $("input#new-name").val();
+ var inputtedPCustomB = $("input#new-name").val();
+ var inputtedPCustomV = $("input#new-name").val();
+ var inputtedPCustomM = $("input#new-name").val();
+
+ newPizza = new Pizza(inputtedFullName, inputtedInitialBalance);
+
+ //code to show users info and current balance
+   $("p#userinfo").append("<span class='userdetailsname'>" + newUser.FullName + "</span>");
+   $("p#Cbalance").append("<span class='userdetailsbalance'>" + newUser.InitialBalance + ksh + "</span>");
+ //hides submit button so users can't add another balance
+   $("button#adduser").removeClass('btn btn-primary');
+   $("button#adduser").addClass('show-contact');
+
+   console.log(newUser.InitialBalance);
 
 });
 
