b2ce8c47176fe0bf8518806e9e0ec9206a2f5c5a
AbugaAroni <abugaaroni@gmail.com>
2020-08-03 16:01:55 +0300

    add basic js scripts to show address collection form

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+
+
+//user interface logic
+
+$(document).ready(function() {
+
+ //when delivery button is clicked, the address collection div should show
+$("#delivery").click( function(event) {
+  event.preventDefault();
+  $("#addressbox").removeClass('show-div');
+});
+
+//when inpick up button is clicked, the address collection div should show
+$("#pickup").click( function(event) {
+ event.preventDefault();
+ $("#").removeClass('show-div');
+});
+
+
+});
