bff3e3968208950d48bb970c4d4c0b155221e593
AbugaAroni <abugaaroni@gmail.com>
2020-08-03 18:20:01 +0300

    add functions and methods to get total price of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,16 @@
 var totalPrice = 0;
 
 //delvery information
-var deliveryinfo = "A delivery cost of 200 has been added";
+var deliveryinfo = "A delivery cost of 200ksh has been added";
 var nodeliveryinfo = "No delivery cost as instore pickup";
 
-
+//collect info about pizza order, this is the pizza object
 function Pizza (size,crust,cheese,type) {
   this.size = size;
   this.crust = crust;
   this.cheese = cheese;
   this.type = type;
+  this.totalprice = 0;
 }
 
 
@@ -24,6 +25,50 @@ function CustomP (base,veggies,meat) {
 }
 
 //price
+Pizza.prototype.pizzaCost = function () {
+  if (this.size === "Small") {
+    this.totalprice += 500;
+  } else if (this.size === "Medium") {
+    this.totalprice += 750;
+  } else if (this.size === "Large") {
+    this.totalprice += 900;
+  }
+  else if (this.size === "Extra-Large") {
+    this.totalprice += 1200;
+  }
+
+  if (this.crust === "Crispy") {
+    this.totalprice += 50;
+  } else if (this.crust === "Stuffed") {
+    this.totalprice += 100;
+  } else if (this.crust === "Glutten-free") {
+    this.totalprice += 150;
+  }
+
+  if (this.cheese === "Normal") {
+    this.totalprice += 50;
+  } else if (this.cheese === "Extra-cheesy") {
+    this.totalprice += 100;
+  } else if (this.cheese === "Vegan-cheese") {
+    this.totalprice += 200;
+  }
+
+  if (this.type === "Margherita") {
+    this.totalprice += 0;
+  } else if (this.type === "Pepperoni") {
+    this.totalprice += 100;
+  } else if (this.type === "Hawaiian") {
+    this.totalprice += 100;
+  }
+  else if (this.type === "Custom") {
+    this.totalprice += 0;
+  }
+  return this.totalprice;
+}
+
+Pizza.prototype.finalCost = function () {
+  totalPrice += this.totalprice;
+}
 
 //user interface logic
 $(document).ready(function() {
@@ -33,6 +78,7 @@ $("#delivery").click( function(event) {
   event.preventDefault();
   $("#addressbox").removeClass('show-div');
   $("button#pickup").addClass('show-div');
+  $("button#delivery").addClass('show-div');
 
   //add delivery cost 200ksh
   totalPrice += 200;
@@ -79,14 +125,25 @@ $("form#pizza-form").submit(function(event) {
  newPizza = new Pizza(inputtedPSize, inputtedPCrust, inputtedPCheese, inputtedPType);
  newPizza = new Customp(inputtedPCustomB, inputtedPCustomV, inputtedPCustomM);
 
+    var pizzaDetails = (inputtedPSize + " - " + inputtedPCrust + ", " + inputtedPCheese + ", " + veggie1 + ", " + veggie2 + ", " + meat);
+    var newPizzaOrder = new Order(customSize, cheese);
+    newPizzaOrder.pizzaCost();
+    totalPriceArray.push(newPizzaOrder.pizzaPrice);
+    $("#pizza-details-dropdown").show();
+    $("#final-cost").text(newPizzaOrder.finalCost());
+    $("#pizza-details").append("<ul><li>" + pizzaDetails + "</li></ul>");
+    $("#size, #sauce, #cheese, #veggie1, #veggie2, #meat").val("");
+  });
+  $("#pizza-details-dropdown").click(function() {
+    $("#pizza-details").toggle();
+
+
  //code to show users info and current balance
    $("p#userinfo").append("<span class='userdetailsname'>" + newUser.FullName + "</span>");
    $("p#Cbalance").append("<span class='userdetailsbalance'>" + newUser.InitialBalance + ksh + "</span>");
  //hides submit button so users can't add another balance
    $("button#adduser").removeClass('btn btn-primary');
    $("button#adduser").addClass('show-contact');
-
-   console.log(newUser.InitialBalance);
 
 });
 
